77a6b4fd35a5ad20ca671c40e195ec6399ad5a74
armandoC27 <armyconte27@gmail.com>
2020-08-10 14:31:14 +0200

    add control in login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
+import {AlertController} from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -7,13 +8,31 @@ import {Router} from '@angular/router';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  constructor(private router: Router) { }
+  username: string;
+  password: string;
+  constructor(private router: Router,
+              public alertController: AlertController) { }
 
   ngOnInit() {
   }
 
-    login() {
+  login() {
+      if (this.username === 'cliente' && this.password === 'cliente'){
         this.router.navigate(['/client']);
-    }
+      } else {
+        this.passwordErrata();
+      }
+  }
+
+  async passwordErrata() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'ATTENZIONE!',
+      subHeader: 'Password errata!',
+      message: 'Controllare nome utente e password.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
 }
