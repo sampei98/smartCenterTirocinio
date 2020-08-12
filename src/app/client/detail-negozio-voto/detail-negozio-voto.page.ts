f42a2fd5c341402724c126f54ff172a639fd9db4
armandoC27 <armyconte27@gmail.com>
2020-08-12 15:14:42 +0200

    update confirm message in recensioni

--- src/app/client/detail-negozio-voto/detail-negozio-voto.page.ts
+++ src/app/client/detail-negozio-voto/detail-negozio-voto.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, NavigationExtras, Router} from '@angular/router';
 import {NegoziServiceService} from '../../services/negozi-service.service';
+import {AlertController} from '@ionic/angular';
 
 @Component({
   selector: 'app-detail-negozio-voto',
@@ -13,7 +14,10 @@ export class DetailNegozioVotoPage implements OnInit {
   valoreRecensione: string;
   testoRecensione: string;
 
-  constructor(private route: ActivatedRoute, private router: Router, private myService: NegoziServiceService)  {
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private myService: NegoziServiceService,
+              public alertController: AlertController)  {
     this.initializaJSONData();
   }
   initializaJSONData(){
@@ -25,7 +29,32 @@ export class DetailNegozioVotoPage implements OnInit {
   }
   ngOnInit() {
   }
-  lasciaRecensione() {
+  async lasciaRecensione() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Confirm!',
+      message: 'Vuoi inserire la recensione?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Si',
+          handler: () => {
+            console.log('Confirm Si');
+            this.aggiungiRecensione();
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+  aggiungiRecensione(){
     console.log(this.valoreRecensione);
     console.log(this.testoRecensione);
     this.data = this.myService.addRecensione(this.data.name, this.valoreRecensione, 'tu', this.testoRecensione, 'oggi');
